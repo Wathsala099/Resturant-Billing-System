d111d2aa545936d5ddbe03cbf2363a588c27db1d
wathsala <wathsalananayakkara0@gmail.com>
2023-12-31 15:35:17 +0530

    First Commit

---
+++ customer/payments_reports.php
@@ -0,0 +1,84 @@
+<?php
+session_start();
+include('config/config.php');
+include('config/checklogin.php');
+check_login();
+require_once('partials/_head.php');
+?>
+
+<body>
+    
+    <?php
+    require_once('partials/_sidebar.php');
+    ?>
+   
+    <div class="main-content">
+      
+        <?php
+        require_once('partials/_topnav.php');
+        ?>
+      
+        <div style="background-image: url(../admin/assets/img/theme/restro00.jpg); background-size: cover;" class="header  pb-8 pt-5 pt-md-8">
+        <span class="mask bg-gradient-dark opacity-8"></span>
+            <div class="container-fluid">
+                <div class="header-body">
+                </div>
+            </div>
+        </div>
+     
+        <div class="container-fluid mt--8">
+        
+            <div class="row">
+                <div class="col">
+                    <div class="card shadow">
+                        <div class="card-header border-0">
+                            Payment Reports
+                        </div>
+                        <div class="table-responsive">
+                            <table class="table align-items-center table-flush">
+                                <thead class="thead-light">
+                                    <tr>
+                                        <th class="text-success" scope="col">Payment Code</th>
+                                        <th scope="col">Payment Method</th>
+                                        <th class="text-success" scope="col">Order Code</th>
+                                        <th scope="col">Amount Paid</th>
+                                        <th class="text-success" scope="col">Date Paid</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <?php
+                                    $customer_id = $_SESSION['customer_id'];
+                                    $ret = "SELECT * FROM  rpos_payments WHERE customer_id ='$customer_id' ORDER BY `created_at` DESC ";
+                                    $stmt = $mysqli->prepare($ret);
+                                    $stmt->execute();
+                                    $res = $stmt->get_result();
+                                    while ($payment = $res->fetch_object()) {
+                                    ?>
+                                        <tr>
+                                            <th class="text-success" scope="row">
+                                                <?php echo $payment->pay_code; ?>
+                                            </th>
+                                            <th scope="row">
+                                                <?php echo $payment->pay_method; ?>
+                                            </th>
+                                            <td class="text-success">
+                                                <?php echo $payment->order_code; ?>
+                                            </td>
+                                            <td>
+                                                $ <?php echo $payment->pay_amt; ?>
+                                            </td>
+                                            <td class="text-success">
+                                                <?php echo date('d/M/Y g:i', strtotime($payment->created_at)) ?>
+                                            </td>
+                                        </tr>
+                                    <?php } ?>
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            
+</body>
+
+</html>
